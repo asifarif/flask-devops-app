af5c3c5f90c38fa34df98cc6ab7aff6ad408818a
asifarif <asif.arif@gmail.com>
2025-04-06 20:58:31 +0500

    main.py has been udpated to get flake8 compliance using pylint

--- app/main.py
+++ app/main.py
@@ -1,13 +1,13 @@
+# main.py - sample file that print a simple message
+"""Basic Flask app for CI/CD learning practice."""
 from flask import Flask
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def hello():
+    """Return a greeting for DevOps learner."""
     return "Hello DevOps Learner! 🌱"
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
-
