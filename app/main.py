fb85c570f34b5989ac936eee7ea8365bcf7afbfb
asifarif <asif.arif@gmail.com>
2025-04-06 21:11:09 +0500

    clearn code with Black Function

--- app/main.py
+++ app/main.py
@@ -6,11 +6,11 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route("/")
 def hello():
     """Return a greeting for DevOps learner."""
     return "Hello DevOps Learner! 🌱"
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
